3baa2e8796a245c10bf91fc1fd65747518827de2
Oscar Wellner <o.wellner@telfort.nl>
2020-09-17 16:28:09 +0200

    Step 3: created layout for start rating view

--- app/src/main/java/com/nonamer777/madlevel3task1/StartFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel3task1/StartFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_start.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -17,5 +19,13 @@ class StartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnStartRating.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_ratingFragment)
+        }
     }
 }
