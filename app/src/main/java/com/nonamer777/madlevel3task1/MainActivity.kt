207c164fc661f4477e5187986139fa29512dd83c
Oscar Wellner <o.wellner@telfort.nl>
2020-09-17 17:25:18 +0200

    minor optimizations

--- app/src/main/java/com/nonamer777/madlevel3task1/MainActivity.kt
+++ app/src/main/java/com/nonamer777/madlevel3task1/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.nonamer777.madlevel3task1
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
